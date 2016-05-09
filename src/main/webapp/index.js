5ec760451652b31f95825502dfb60953b51564d6
王天硕 <nelsonnick@126.com>
2016-05-09 22:57:43 +0800

    init

---
+++ src/main/webapp/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+var HelloWorld = React.createClass({
+    render: function () {return (<div>Hello WTS</div>);}
+});
+
+ReactDOM.render(<HelloWorld/>,document.getElementById('test'));
