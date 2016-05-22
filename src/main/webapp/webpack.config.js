1f92797ffa36100a63250c1d11600ee09a593dcc
王天硕 <nelsonnick@126.com>
2016-05-22 20:10:27 +0800

    add

---
+++ src/main/webapp/webpack.config.js
@@ -0,0 +1,54 @@
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+var webpack = require('webpack');
+
+var plugins = [
+    new CopyWebpackPlugin([
+        {from: './index.html', to: './index.html'}
+    ]),
+    //第三方库自动注入
+    new  webpack.ProvidePlugin({
+        '_':'lodash',
+        '$':'jquery',
+        'React':'react',
+        'ReactDOM':'react-dom'
+    })
+];
+/*module.exports = function (webpackConfig) {
+    webpackConfig.babel.plugins.push(['antd', {
+        style: 'css', // if true, use less
+    }]);
+
+    // Fix ie8 compatibility
+    webpackConfig.module.loaders.unshift({
+        test: /\.jsx?$/,
+        loader: 'es3ify-loader',
+    });
+
+    return webpackConfig;
+};*/
+module.exports = {
+    entry: {
+        mainPage: './mainPage.js'
+    },
+    output: {
+        path: './build/',
+        filename: '[name].js'
+    },
+
+    plugins: plugins,
+
+    module: {
+        loaders: [
+
+            {test: /(\.js$|\.jsx$)/,
+                exclude: /node_modules/,
+                loader: 'babel',
+                query: {
+                    presets: ['react', 'es2015']
+                }
+            },
+            { test: /\.css$/, loader: "style!css" },
+            {test: /\.less/,loader: 'style-loader!css-loader!less-loader'}
+        ]
+    }
+};
